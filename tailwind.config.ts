ad99372a533713090ef8ef3f0fce20548df36637
Pavel Stepanov <pas273@miami.edu>
2025-01-07 19:23:12 -0500

    Refresh
    Refresh

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,3 @@
-// tailwind.config.ts
-
 import type { Config } from "tailwindcss";
 
 export default {
@@ -8,19 +6,11 @@ export default {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  darkMode: 'class', // Important: Enable class-based dark mode
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
-      },
-      fontFamily: {
-        'realtime-black': ['Realtime-Black', 'Arial', 'sans-serif'],
-        'realtime-bold': ['Realtime-Bold', 'Arial', 'sans-serif'],
-        'realtime-light': ['Realtime-Light', 'Arial', 'sans-serif'],
-        'realtime-regular': ['Realtime-Regular', 'Arial', 'sans-serif'],
-        'realtime-semibold': ['Realtime-SemiBold', 'Arial', 'sans-serif'],
       },
     },
   },
--- types/project.ts
+++
@@ -1,8 +0,0 @@
-export interface Project {
-    image: string;
-    title: string;
-    description: string;
-    tags: string[];
-    github?: string;
-    demo?: string;
-  }
